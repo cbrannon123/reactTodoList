3dc9b491c2f22a5507a98bf2a61036ab11f7e1a3
Casey Brannon <cbrannon412@gmail.com>
2019-07-13 00:27:54 -0700

    not another to-do list!

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return(
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Todo list is a crash</p>
+        </React.Fragment>
+    )
+}
+
+export default About
